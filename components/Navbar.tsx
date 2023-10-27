66503f9d4779e1fd74c95cfbd575395c5ff51b1f
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-10-28 02:05:41 +0530

    mobile sidebar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import { MobileSidebar } from "./MobileSidebar";
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
