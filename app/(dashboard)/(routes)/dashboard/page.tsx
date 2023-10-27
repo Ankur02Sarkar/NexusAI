c621f35b23ee25947878266b7d0a02dfa1b0d0d4
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-10-28 01:54:23 +0530

    sidebar done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import React from "react";
 
 const DashboardPage = () => {
   return (
-    <Button variant="destructive" size="lg">Dashboard</Button>
-  )
-}
+    <div>
+      <p>Dashboard Page</p>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nexus AI",
+  title: "NexusAI",
   description: "Your AI Assistant",
 };
 
